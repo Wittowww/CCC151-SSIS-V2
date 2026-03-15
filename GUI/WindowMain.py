114d1f764caa5ca14e107c170095cabf7a6560bc
Niezsa.Bacus <bacusjuniezsa@gmail.com>
2026-03-15 20:20:16 +0800

    for now the basics, yayyy

--- GUI/WindowMain.py
+++ GUI/WindowMain.py
@@ -1,10 +1,14 @@
 import os
 
-from PySide6.QtWidgets import ( QApplication, QMainWindow, QWidget, QDialog, QHBoxLayout, QVBoxLayout )
+from PySide6.QtWidgets import ( QApplication, QMainWindow, QWidget, QHBoxLayout, QVBoxLayout, QLabel, QPushButton, QStackedWidget )
 from PySide6.QtGui import QIcon, QAction
 from PySide6.QtCore import Qt
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+from USERVIEW.DataTable import StudentTable
+from USERVIEW.DataTable import ProgramTable
+from USERVIEW.DataTable import CollegeTable
 
 
 class mainApp(QMainWindow):
@@ -26,5 +30,50 @@ class mainApp(QMainWindow):
 
     def main_UI (self, central_widget):
         layout_Main = QVBoxLayout(central_widget)
+
+        #Label / name 
+        TITLElable = QLabel("Simple System Information System")
+        TITLElable.setFixedHeight(70)
+
+        #Page Picker for Tables
+
+        #layout thingsz
+        upperBox_container = QWidget()
+        upperBox_container.setFixedHeight(70)
+
+        upperBox = QHBoxLayout(upperBox_container)
+
+        self.Students_button = QPushButton ("Student")
+        self.Programs_button = QPushButton ("Program")
+        self.Colleges_button = QPushButton ("College")
+        #makes sure button shows up
+        upperBox.addWidget(self.Students_button)
+        upperBox.addWidget(self.Programs_button)
+        upperBox.addWidget(self.Colleges_button)
+        #button actions
+        #self.Students_button.clicked.connect()
+        #self.Programs_button.clicked.connect()
+        #self.Colleges_button.clicked.connect()
+
+        upperBox.addStretch()
+
+        #Layout for the table pages
+        TablePage = QStackedWidget()
+        TablePage.setMinimumHeight(580)
+
+
+        TablePage.addWidget((StudentTable()))
+        TablePage.addWidget((ProgramTable()))
+        TablePage.addWidget((CollegeTable()))
+
+        layout_Main.addWidget(TITLElable)
+        layout_Main.addWidget(upperBox_container)
+        layout_Main.addWidget(TablePage)
+
+
+
+
+
+
 
 
---
+++ USERVIEW/DataTable.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+from PySide6.QtWidgets import (QWidget, QTableWidget, QTableWidgetItem )
+
+class StudentTable(QWidget):
+    def __init__(self ):
+        super().__init__()
+
+
+class ProgramTable(QWidget):
+    def __init__(self):
+        super().__init__()
+
+class CollegeTable(QWidget):
+    def __init__(self):
+        super().__init__()
